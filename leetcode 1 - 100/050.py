b8008b8ca90846d635999ddc2d660f23675f6563
2020131024 <singcrazy@naver.com>
2024-06-01 16:41:19 +0900

    Update 050.py

--- leetcode 1 - 100/050.py
+++ leetcode 1 - 100/050.py
@@ -1,9 +1,4 @@
 class Solution:
-    # AC, fast
-    # def myPow(self, x: float, n: int) -> float:
-    #     return x**n
-
-    # AC, Implement pow
     def myPow(self, x: float, n: int) -> float:
         if n < 0:
             x = 1/x
@@ -23,23 +18,3 @@ class Solution:
                 return resultDict[n]
 
         return Pow(n)
-
-    # MemoryError
-    # x = -1.00000, n = -2147483648
-    # x = 1.00001, n = 123456
-    # def myPow(self, x: float, n: int) -> float:
-    #     if n < 0:
-    #         x = 1/x
-    #         n = abs(n)
-
-    #     if n == 0:
-    #         return 1
-
-        
-    #     result = [x] * n
-    #     while len(result) > 1:
-    #         resultLen = len(result)
-    #         result = [result[i]*result[i+1] if i+1 < resultLen else result[i] for i in range(0, resultLen, 2)]
-            
-    #     return result[0]
-

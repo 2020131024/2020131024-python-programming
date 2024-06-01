5859b887f291b111e2d0519abf19f9c9256b0852
2020131024 <singcrazy@naver.com>
2024-06-01 16:41:44 +0900

    Update 055.py

--- leetcode 1 - 100/055.py
+++ leetcode 1 - 100/055.py
@@ -10,22 +10,3 @@ class Solution:
                 break
 
         return right >= target
-
-
-    # AC, slow
-    # def canJump(self, nums: List[int]) -> bool:
-    #     if len(nums) == 1:
-    #         return True
-
-    #     numsLen = len(nums)
-    #     flag = [False] * numsLen
-    #     flag[0] = True
-
-    #     for i, n in enumerate(nums[:-1]):
-    #         if flag[i]:
-    #             for j in range(i+1, min(i+1+n, numsLen)):
-    #                 flag[j] = True
-    #             if flag[-1]:
-    #                 break
-        
-    #     return flag[-1]

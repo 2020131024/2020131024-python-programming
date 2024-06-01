83842fffafcc42832a8791dcc153acc967a70d35
2020131024 <singcrazy@naver.com>
2024-06-01 15:14:31 +0900

    Update 0070_E_Climbing Stairs.py

--- leetcode 1 - 100/0070_E_Climbing Stairs.py
+++ leetcode 1 - 100/0070_E_Climbing Stairs.py
@@ -1,13 +1,4 @@
 class Solution:
-    # AC, slow, but uses very little memory.
-    # def climbStairs(self, n: int) -> int:
-    #     lastTwo, lastOne = 1, 2
-    #     for i in range(n-2):
-    #         lastTwo, lastOne = lastOne, lastTwo + lastOne
-
-    #     return lastOne if n > 1 else lastTwo
-
-    # AC, fast, but uses too much memory.
     def climbStairs(self, n: int) -> int:
         result = [0, 1, 2]
         resultLen = len(result)

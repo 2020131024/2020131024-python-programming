d21e3b7480f29eb5ea916ffe17c8587cf6f41eaa
2020131024 <singcrazy@naver.com>
2024-06-01 16:40:44 +0900

    Update 045.py

--- leetcode 1 - 100/045.py
+++ leetcode 1 - 100/045.py
@@ -9,14 +9,3 @@ class Solution:
                 end = maxDist
                 jump += 1
         return jump
-
-
-    # def jump(self, nums: List[int]) -> int:
-    #     result = [len(nums)*2] * len(nums)
-    #     result[0] = 0
-
-    #     for i in range(len(nums) - 1):
-    #         for j in range(i + 1, min(i + nums[i] + 1, len(nums))):
-    #             result[j] = min(result[j], result[i] + 1)
-
-    #     return result[-1]

f4f4dc7c6a45da7414d7a8ede9682ce47dfc4c8c
2020131024 <singcrazy@naver.com>
2024-06-01 16:48:58 +0900

    Delete leetcode 101 - 200 directory

--- leetcode 101 - 200/0118_E_Pascal's Triangle.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        result = [[1]]
-        lastRow = result[0]
-        for row in range(1, numRows):
-            lastRow = [sum(v) for v in zip([0] + lastRow, lastRow + [0])]
-            result.append(lastRow)
-        return result

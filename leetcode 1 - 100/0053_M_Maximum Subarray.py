287a95664ed60e265b41cb0decba301caf6e4e2c
2020131024 <singcrazy@naver.com>
2024-06-01 16:14:42 +0900

    Delete leetcode 1 - 100 directory

--- leetcode 1 - 100/0053_M_Maximum Subarray.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        maxSub = nums[0]
-        last = nums[0]
-        for num in nums[1:]:
-            last = max(last + num, num)
-            maxSub = max(maxSub, last)
-            
-        return maxSub
--- leetcode 1 - 100/0070_E_Climbing Stairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        result = [0, 1, 2]
-        resultLen = len(result)
-
-        for i in range(resultLen, n + 1):
-            result.append(result[i-2] + result[i-1])
-        
-        return result[n]

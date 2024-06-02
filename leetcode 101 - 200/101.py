6dec692cb9e8fac9cd985abee245516e7bf7cfbe
2020131024 <singcrazy@naver.com>
2024-06-02 23:18:31 +0900

    Update 101.py

--- leetcode 101 - 200/101.py
+++ leetcode 101 - 200/101.py
@@ -5,7 +5,6 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # AC, fast
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
         def checkSymmetric(node_1, node_2):
@@ -20,29 +19,3 @@ class Solution:
             return checkSymmetric(node_1.left, node_2.right) and checkSymmetric(node_1.right, node_2.left)
         
         return checkSymmetric(root.left, root.right)
-
-    # AC, slow
-    # def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-
-    #     layer = [root]
-    #     while layer:
-    #         nextLayer = []
-    #         for node in layer:
-    #             if node:
-    #                 nextLayer.extend([node.left, node.right])
-    #             else:
-    #                 nextLayer.extend([None, None])
-
-    #         for i in range(len(nextLayer)//2):
-    #             val_1 = None if nextLayer[i] is None else nextLayer[i].val
-    #             val_2 = None if nextLayer[-(i+1)] is None else nextLayer[-(i+1)].val
-                
-    #             if val_1 != val_2:
-    #                 return False
-
-    #         layer = None
-    #         if any(node is not None for node in nextLayer):
-    #             layer = nextLayer
-            
-    #     return True
-            

01f46e496323bbccc4c442c289e1784a40eb1040
2020131024 <singcrazy@naver.com>
2024-06-02 23:18:46 +0900

    Update 102.py

--- leetcode 101 - 200/102.py
+++ leetcode 101 - 200/102.py
@@ -22,25 +22,3 @@ class Solution:
             nodeList = nextNodeList
 
         return result[:-1]
-
-    # def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-    #     result = []
-    #     if root is None:
-    #         return result
-
-    #     nodeList = [root]
-    #     while nodeList:
-    #         result.append([node.val for node in nodeList])
-
-    #         nextNodeList = []
-    #         for node in nodeList:
-    #             if node.left:
-    #                 nextNodeList.append(node.left)
-    #             if node.right:
-    #                 nextNodeList.append(node.right)
-
-    #         nodeList = None
-    #         if len(nextNodeList) > 0:
-    #             nodeList = nextNodeList
-
-    #     return result

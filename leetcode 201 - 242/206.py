c5f070cbefa7415af470ac0414a43d218bc87ff1
2020131024 <singcrazy@naver.com>
2024-06-18 16:14:34 +0900

    Update 206.py

--- leetcode 201 - 242/206.py
+++ leetcode 201 - 242/206.py
@@ -25,28 +25,3 @@ class Solution:
                 return False
 
         return True
-
-        
-
-    # AC, slow
-    # def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-    #     preDict = collections.defaultdict(set)
-    #     for a, b in prerequisites:
-    #         preDict[a].add(b)
-        
-    #     finish = set()
-
-    #     def check(course):
-    #         if len(preDict[course]) == 0:
-    #             finish.add(course)
-    #             for j in preDict:
-    #                 if course in preDict[j]:
-    #                     preDict[j].discard(course)
-    #                     check(j)
-
-    #     for i in range(numCourses):
-    #         check(i)
-
-    #     # print(finish)
-
-    #     return len(finish) == numCourses

0c0d95db9ca37c488d109a95ba7f10ff5859d229
2020131024 <singcrazy@naver.com>
2024-06-18 16:14:18 +0900

    Update 204.py

--- leetcode 201 - 242/204.py
+++ leetcode 201 - 242/204.py
@@ -12,8 +12,3 @@ class Solution:
                 if (not pair1) or (not pair2) or charDict1[c1] != c2 or charDict2[c2] != c1:
                     return False
         return True
-
-    # slow
-    # def isIsomorphic(self, s: str, t: str) -> bool:
-    #     charSet1, charSet2 = set(s), set(t)
-    #     return len(charSet1) == len(charSet2) == len(set(zip(s, t)))
